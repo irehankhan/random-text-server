f2763976f9ffb1998af6e383a55182d90fb182e0
irehankhan <irehankhan@yahoo.com>
2023-12-08 16:37:52 +0500

    Modification

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,10 +2,50 @@ resource "aws_instance" "BACKEND-SERVER" {
     ami = "ami-0fc5d935ebf8bc3bc"
     instance_type = "t2.micro"
     key_name = aws_key_pair.ssh_key.key_name
+
+    security_groups = [aws_security_group.random_text_sg.name]
+
     count=1
 
     tags = {
         Name = "Server Instance"
 
     }
+}
+
+resource "aws_security_group" "random_text_sg" {
+  name        = "random_text_sg"
+  description = "Allow inbound traffic on port 5010, 12345, and 22 from all IPs"
+
+  # VPC ID where the security group will be created
+  vpc_id = "vpc-003893cb3140f5d73"
+#Client
+  # Inbound rules to allow traffic on ports 5010, 12345, and 22 from all IPs
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+# Server Port
+  ingress {
+    from_port   = 12345
+    to_port     = 12345
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+# For SSH Access
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+# Outbound rule to allow all traffic to all IPS
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
